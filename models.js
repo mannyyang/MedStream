1640768dc9c989d78d102ee56c77cdbffa739e36
Kevin T. Lee <kevin.lee14@gmail.com>
2014-01-29 20:46:01 -0800

    fixed merge issues, updated rss template

--- models.js
+++ models.js
@@ -4,22 +4,6 @@ var textSearch = require('mongoose-text-search');
 var Schema = mongoose.Schema;
 
 var mediaSchema = new Schema ({
-<<<<<<< HEAD
-        id: {type: String, index: {unique: true, dropDups: true}},
-        created_at: Date,
-        user: [{
-                id: Number,
-                name: String,
-                screen_name: String,
-                location: String
-        }],
-        title: String,
-        text: String,
-        link: String,
-        source: String,
-        keywords: [],
-        polarity: Number
-=======
 	id: {type: String, index: {unique: true, dropDups: true}},
 	created_at: Date,
 	user: [{
@@ -34,7 +18,6 @@ var mediaSchema = new Schema ({
 	source: String,
 	keywords: [],
 	polarity: Number
->>>>>>> 40b07a81a88792aed65086efb68147836fd61751
 });
 
 // give our schema text search capabilities
